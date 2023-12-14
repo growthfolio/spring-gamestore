fcc2ddb9a7e27d329856d63d7374db0a3bb4dd48
felipe-jaque-dev <felipealexandrej@gmail.com>
2023-12-14 12:57:46 -0300

    Adicionado configurações iniciais do projeto como banco de dados e dependências

---
+++ src/main/java/com/energygames/lojadegames/EnergygamesApplication.java
@@ -0,0 +1,13 @@
+package com.energygames.lojadegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergygamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergygamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/energygames/lojadegames/EnergygamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energygames.lojadegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergygamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
